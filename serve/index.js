d8eee1fc1b8447c6ebc82d643d4a39d6aa27b98b
WindDancer <winddancer@WindDancerMacBook.local>
2020-12-19 20:34:34 +0800

    心跳检测与断线重连

--- serve/index.js
+++ serve/index.js
@@ -1,8 +1,4 @@
 /*
- * @file: 文件描述
- * @author: huangjitao
- */
-/*
  * @file: websocket服务端
  * @author: huangjitao
  */
@@ -16,8 +12,11 @@ const wss = new WebSocket.Server({port: 3000})
 // const wss = new WebSocket.Server({noServer: true})
 
 let group = {} // 存放每个房间的在线人数
+const timeInterval = 3000 // 设置一秒的心跳检测间隔
 wss.on('connection', function connection(ws) {
   console.log('one client is connected');
+  // 初识的心跳连接状态设置为在线
+  ws.isAlive = true
   // 接受客户端的消息
   ws.on('message', function(msg) {
     const msgObj = JSON.parse(msg)
@@ -39,21 +38,24 @@ wss.on('connection', function connection(ws) {
           // 返回前台鉴权失败消息
           msgObj.message = '鉴权失败'
           msgObj.isAuth = false
-          // msgObj.event = 'noauth'
           ws.send(JSON.stringify(msgObj))
         } else {
           // 鉴权通过
           ws.isAuth = true
           msgObj.isAuth = true
           msgObj.message = '鉴权成功'
-          // msgObj.event = 'noauth'
           ws.send(JSON.stringify(msgObj))
         }
       })
       return
     }
-    // 判断是否鉴权成功 
+    // 判断是否鉴权成功，如果失败，不执行消息的发送
     if (!ws.isAuth) {
+      return
+    }
+    // 心跳检测，接受到客户端信息时，将客户端状态置为在线
+    if (msgObj.event === 'heartbeat' && msgObj.message === 'pong') {
+      ws.isAlive = true
       return
     }
     // 广播消息
@@ -82,6 +84,24 @@ wss.on('connection', function connection(ws) {
     })
   })
 })
+
+setInterval(() => {
+  wss.clients.forEach((ws) => {
+    if (!ws.isAlive && ws.roomid) {
+      // 在一定的时间内客户端的状态仍为离线时，中断与客户端的ws连接
+      group[ws.roomid] --
+      delete ws['roomid']
+      return ws.terminate()
+    }
+    // 每隔一定的时间，发送心跳检测
+    ws.isAlive = false
+    ws.send(JSON.stringify({
+      event: 'heartbeat',
+      message: 'ping',
+      num: group[ws.roomid]
+    }))
+  })
+}, timeInterval);
 
 // // 'upgrade'将http服务升级到包含websocket
 // server.on('upgrade', function upgrade(request, socket, head) {
